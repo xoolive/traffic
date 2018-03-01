f072568998a97c9575cbd4269d3fb1083f47cd96
Xavier Olive <git@xoolive.org>
2018-03-01 18:25:01 +0100

    more examples, sample data

---
+++ examples/clip_so6.py
@@ -0,0 +1,61 @@
+import argparse
+from pathlib import Path
+
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from tqdm import tqdm
+from traffic.data import sectors
+from traffic.data.airac import Sector
+from traffic.so6 import SO6
+
+
+def clip(so6: SO6, sector: Sector) -> SO6:
+    return so6.inside_bbox(sector).intersects(sector)
+
+def unpack_and_clip(filename: str, sectorname: str) -> SO6:
+    so6 = SO6.parse_so6(filename)
+    sector = sectors[sectorname]
+    if sector is None:
+        raise ValueError("Sector not found")
+    return clip(so6, sector)
+
+def prepare_all(filename: Path, output_dir: Path, sectorname: str) -> None:
+    so6 = unpack_and_clip(filename.as_posix(), sectorname)
+    output_name = filename.with_suffix('.pkl').name
+    so6.to_pkl((output_dir / output_name).as_posix())
+
+def glob_all(directory: Path, output_dir: Path, sectorname: str,
+             max_workers: int=4) -> None:
+
+    if not directory.is_dir():
+        raise ValueError(f"Directory {directory} does not exist")
+
+    if not output_dir.is_dir():
+        output_dir.mkdir(parents=True)
+
+    with ProcessPoolExecutor(max_workers=max_workers) as executor:
+        tasks = {executor.submit(prepare_all, filename, output_dir, sectorname):
+                 filename for filename in directory.glob('**/*.so6')}
+        for future in tqdm(as_completed(tasks), total=len(tasks)):
+            try:
+                future.result()
+            except Exception as e:
+                print(f"Exception {e} occurred on file {tasks[future]}")
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="Clip SO6 on sector")
+
+    parser.add_argument("-d", dest="directory", type=Path,
+                        help="directory containing so6 files")
+    parser.add_argument("-o", dest="output_dir", type=Path,
+                        help="output directory for pkl files")
+    parser.add_argument("-s", dest="sectorname",
+                        help="name of the sector to pick in AIRAC files")
+    parser.add_argument("-t", dest="max_workers", default=4, type=int,
+                        help="number of parallel processes")
+
+
+    args = parser.parse_args()
+
+    glob_all(**vars(args))
