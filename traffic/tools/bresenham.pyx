0af3d6ebe2382a9784bab0957471445710f09d11
Xavier Olive <git@xoolive.org>
2018-04-25 11:04:46 +0200

    extension bresenham

--- traffic/tools/bresenham.pyx
+++ traffic/tools/bresenham.pyx
@@ -2,7 +2,8 @@
 cimport cython
 
 @cython.boundscheck(False)
-cdef int grid_increment(long x0, long y0, long x1, long y1, long[:, :] grid):
+cdef int grid_increment(long x0, long y0, long x1, long y1, long mult,
+                        long[:, :] grid):
 
     cdef unsigned nrows, ncols
     cdef long e2, sx, sy, err
@@ -26,7 +27,7 @@ cdef int grid_increment(long x0, long y0, long x1, long y1, long[:, :] grid):
             break
 
         if (0 <= x0 < nrows) and (0 <= y0 < ncols):
-            grid[x0, y0] += 1
+            grid[x0, y0] += mult
 
         if x0 == x1 and y0 == y1:
             break
@@ -69,9 +70,44 @@ def bresenham(long[:, :] points, long[:, :] grid):
         for k in range(points.shape[0] - 1):
             x1 = points[k+1, 0]
             y1 = points[k+1, 1]
-            grid_increment(points[k, 0], points[k, 1], x1, y1, grid)
+            grid_increment(points[k, 0], points[k, 1], x1, y1, 1, grid)
 
         if 0 <= x1 < grid.shape[0] and 0 <= y1 < grid.shape[1]:
             # Count the last point in the curve.
             grid[x1, y1] += 1
+
+
+def bresenham_multiply(long[:, :] points, long[:] mult, long[:, :] grid):
+    """bresenham(long[:, :] points, long[:] mult, long[:, :] grid)
+    Bresenham's algorithm.
+
+    - points is a memory view over a 2D numpy array (the trajectory)
+    - mult is a memory view over a 1D numpy array (vertrate, bearing, etc.)
+    - grid is a memory view over a 2D numpy array (the grid)
+
+    The algorithms adds 1 to all the cells the trajectory cross.
+
+    >>> import numpy as np
+    >>> points = np.array([[0, 0], [1, 11]], dtype=np.int64)
+    >>> grid = np.zeros((2, 11), dtype=np.int64)
+    >>> bresenham(points, grid)
+    >>> grid
+    array([[1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1]])
+
+    See also: http://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+    """
+
+    cdef unsigned k = 0
+    cdef int x1 = 0, y1 = 0
+
+    with cython.boundscheck(False):
+        for k in range(points.shape[0] - 1):
+            x1 = points[k+1, 0]
+            y1 = points[k+1, 1]
+            grid_increment(points[k, 0], points[k, 1], x1, y1, mult[k], grid)
+
+        if 0 <= x1 < grid.shape[0] and 0 <= y1 < grid.shape[1]:
+            # Count the last point in the curve.
+            grid[x1, y1] += mult[k]
 
