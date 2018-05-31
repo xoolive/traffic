b13308e1714cd4d4108ffb56d068a5c73ddf14b4
Xavier Olive <git@xoolive.org>
2018-05-31 16:37:30 +0200

    some cleaning

--- traffic/data/__init__.py
+++ traffic/data/__init__.py
@@ -24,6 +24,7 @@ config_file = config_dir / "traffic.conf"
 if not config_dir.exists():
     config_dir.mkdir()
     with config_file.open('w') as fh:
+        # TODO prepare a bit more
         fh.write("[global]\nairac_path = ")
 
 if not cache_dir.exists():
@@ -36,28 +37,28 @@ airac_path_str = config.get("global", "airac_path", fallback="")
 if airac_path_str != "":
     SectorParser.airac_path = Path(airac_path_str)
 
-SectorParser.cache_dir = cache_dir
-FlightRadar24.username = config.get("global", "fr24_username", fallback="")
-FlightRadar24.password = config.get("global", "fr24_password", fallback="")
-FlightRadar24.cache_dir = cache_dir / "flightradar24"
-
-AirportParser.cache = cache_dir / "airports.pkl"
-NavaidParser.cache = cache_dir / "navaids.pkl"
-Airways.cache = cache_dir / "airways.pkl"
 Aircraft.cache = cache_dir / "aircraft.pkl"
+AirportParser.cache = cache_dir / "airports.pkl"
+Airways.cache = cache_dir / "airways.pkl"
+NavaidParser.cache = cache_dir / "navaids.pkl"
+SectorParser.cache_dir = cache_dir
 
-OpenSky.cache_dir = cache_dir / "impala"
-# not great here...
+aircraft = Aircraft()
+airports = AirportParser()
+airways = Airways()
+navaids = NavaidParser()
+sectors = SectorParser(config_file)
+
+OpenSky.cache_dir = cache_dir / "opensky"
+# TODO not great here...
 if not OpenSky.cache_dir.exists():
     OpenSky.cache_dir.mkdir(parents=True)
-
-sectors = SectorParser(config_file)
-airports = AirportParser()
-navaids = NavaidParser()
-flightradar24 = FlightRadar24()
-airways = Airways()
-aircraft = Aircraft()
 
 opensky_username = config.get("global", "opensky_username", fallback="")
 opensky_password = config.get("global", "opensky_password", fallback="")
 opensky = OpenSky(opensky_username, opensky_password)
+
+FlightRadar24.username = config.get("global", "fr24_username", fallback="")
+FlightRadar24.password = config.get("global", "fr24_password", fallback="")
+FlightRadar24.cache_dir = cache_dir / "flightradar24"
+flightradar24 = FlightRadar24()
