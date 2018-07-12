9659cf473ff9c68f71be64df2a3a28afb6d327f3
Xavier Olive <git@xoolive.org>
2018-07-12 10:05:46 +0200

    better interface

--- traffic/data/adsb/opensky.py
+++ traffic/data/adsb/opensky.py
@@ -17,29 +17,53 @@ from ..basic.airport import Airport
 
 class OpenSky(object):
 
-    columns = ["icao24", "callsign", "origin_country", "last_position",
-               "timestamp", "longitude", "latitude", "altitude", "onground",
-               "ground_speed", "track", "vertical_rate", "sensors",
-               "baro_altitude", "squawk", "spi", "position_source"]
+    _impala_columns = [
+        "time",
+        "icao24",
+        "lat",
+        "lon",
+        "velocity",
+        "heading",
+        "vertrate",
+        "callsign",
+        "onground",
+        "alert",
+        "spi",
+        "squawk",
+        "baroaltitude",
+        "geoaltitude",
+        "lastposupdate",
+        "lastcontact",
+        # "serials", keep commented, array<int>
+        "hour",
+    ]
 
-    basic_request = ("select * from state_vectors_data4 {other_columns} "
-                     "where hour>={before_hour} and hour<{after_hour} "
-                     "and time>={before_time} and time<{after_time} "
-                     "{other_where}")
+    _json_columns = [
+        "icao24",
+        "callsign",
+        "origin_country",
+        "last_position",
+        "timestamp",
+        "longitude",
+        "latitude",
+        "altitude",
+        "onground",
+        "ground_speed",
+        "track",
+        "vertical_rate",
+        "sensors",
+        "baro_altitude",
+        "squawk",
+        "spi",
+        "position_source",
+    ]
 
-    airport_request = ("select icao24, callsign from state_vectors_data4 "
-                       "where lat<={airport_latmax} and lat>={airport_latmin} "
-                       "and lon<={airport_lonmax} and lon>={airport_lonmin} "
-                       "and baroaltitude<=1000 "
-                       "and hour>={before_hour} and hour<{after_hour} "
-                       "and time>={before_time} and time<{after_time} "
-                       "group by icao24, callsign")
-
-    sensor_request = ("select icao24, callsign, s.ITEM as serial,"
-                      "count(*) as count from "
-                      "state_vectors_data4, state_vectors_data4.serials s "
-                      "where hour>={before_hour} and hour<{after_hour} "
-                      "{other_where} group by icao24, callsign, s.ITEM")
+    basic_request = (
+        "select {columns} from state_vectors_data4 {other_tables} "
+        "where hour>={before_hour} and hour<{after_hour} "
+        "and time>={before_time} and time<{after_time} "
+        "{other_params}"
+    )
 
     shell: Optional[Any]  # the connection to the Impala shell
 
@@ -59,12 +83,13 @@ class OpenSky(object):
             self.auth = (username, password)
 
     def clear_cache(self) -> None:
-        for file in self.cache_dir.glob('*'):
+        for file in self.cache_dir.glob("*"):
             file.unlink()
 
     @staticmethod
-    def _format_dataframe(df: pd.DataFrame,
-                          nautical_units=True) -> pd.DataFrame:
+    def _format_dataframe(
+        df: pd.DataFrame, nautical_units=True
+    ) -> pd.DataFrame:
 
         df.callsign = df.callsign.str.strip()
 
@@ -81,7 +106,7 @@ class OpenSky(object):
         pd.options.mode.chained_assignment = None
         df = df.loc[df.last_position.notnull()]  # do we really miss much?
         df.last_position = df.last_position.apply(datetime.fromtimestamp)
-        df = df.sort_values('timestamp')
+        df = df.sort_values("timestamp")
 
         return df
 
@@ -90,8 +115,12 @@ class OpenSky(object):
             raise RuntimeError("This method requires authentication.")
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect("data.opensky-network.org", port=2230,
-                       username=self.username, password=self.password)
+        client.connect(
+            "data.opensky-network.org",
+            port=2230,
+            username=self.username,
+            password=self.password,
+        )
         self.shell = client.invoke_shell()
         self.connected = True
         total = ""
@@ -99,10 +128,15 @@ class OpenSky(object):
             b = self.shell.recv(256)
             total += b.decode()
 
-    def _impala(self, request: str) -> Optional[pd.DataFrame]:
+    def _impala(
+        self, request: str, cached: bool = True
+    ) -> Optional[pd.DataFrame]:
 
-        digest = hashlib.md5(request.encode('utf8')).hexdigest()
+        digest = hashlib.md5(request.encode("utf8")).hexdigest()
         cachename = self.cache_dir / digest
+
+        if not cached:
+            cachename.unlink()
 
         if not cachename.exists():
             if not self.connected:
@@ -117,11 +151,11 @@ class OpenSky(object):
             while len(total) == 0 or total[-19:] != "[hadoop-1:21000] > ":
                 b = self.shell.recv(256)
                 total += b.decode()
-            with cachename.open('w') as fh:
+            with cachename.open("w") as fh:
                 fh.write(total)
 
         logging.info("Reading request in cache {}".format(cachename))
-        with cachename.open('r') as fh:
+        with cachename.open("r") as fh:
             s = StringIO()
             count = 0
             for line in fh.readlines():
@@ -136,14 +170,21 @@ class OpenSky(object):
 
         return None
 
-    def history(self, before: timelike, after: Optional[timelike]=None,
-                date_delta: timedelta=timedelta(hours=1),
-                callsign: Optional[Union[str, Iterable[str]]]=None,
-                serials: Optional[Iterable[int]]=None,
-                bounds: Optional[Tuple[float, float, float, float]]=None,
-                other_columns: str="", other_where: str="",
-                progressbar: Callable[[Iterable], Iterable]=iter
-                ) -> Optional[Union[Traffic, Flight]]:
+    def history(
+        self,
+        before: timelike,
+        after: Optional[timelike] = None,
+        *args,
+        date_delta: timedelta = timedelta(hours=1),
+        callsign: Optional[Union[str, Iterable[str]]] = None,
+        serials: Optional[Iterable[int]] = None,
+        bounds: Optional[Tuple[float, float, float, float]] = None,
+        other_tables: str = "",
+        other_params: str = "",
+        progressbar: Callable[[Iterable], Iterable] = iter,
+        cached: bool = True,
+        count: bool = False,
+    ) -> Optional[Union[Traffic, Flight]]:
 
         return_flight = False
         before = to_datetime(before)
@@ -153,16 +194,16 @@ class OpenSky(object):
             after = before + timedelta(days=1)
 
         if isinstance(serials, Iterable):
-            other_columns += ", state_vectors_data4.serials s "
-            other_where += "and s.ITEM in {} ".format(tuple(serials))
+            other_tables += ", state_vectors_data4.serials s "
+            other_params += "and s.ITEM in {} ".format(tuple(serials))
 
         if isinstance(callsign, str):
-            other_where += "and callsign='{:<8s}' ".format(callsign)
+            other_params += "and callsign='{:<8s}' ".format(callsign)
             return_flight = True
 
         elif isinstance(callsign, Iterable):
             callsign = ",".join("'{:<8s}'".format(c) for c in callsign)
-            other_where += "and callsign in ({}) ".format(callsign)
+            other_params += "and callsign in ({}) ".format(callsign)
 
         if bounds is not None:
             try:
@@ -172,60 +213,84 @@ class OpenSky(object):
             except AttributeError:
                 west, south, east, north = bounds
 
-            other_where += "and lon>={} and lon<={} ".format(west, east)
-            other_where += "and lat>={} and lat<={} ".format(south, north)
+            other_params += "and lon>={} and lon<={} ".format(west, east)
+            other_params += "and lat>={} and lat<={} ".format(south, north)
 
         cumul = []
         sequence = list(split_times(before, after, date_delta))
+        columns = ", ".join(self._impala_columns)
+
+        if count is True:
+            other_params += "group by " + columns
+            columns = "count(*) as count, " + columns
+            other_tables += ", state_vectors_data4.serials s"
 
         for bt, at, bh, ah in progressbar(sequence):
 
             logging.info(
                 f"Sending request between time {bt} and {at} "
-                f"and hour {bh} and {ah}")
+                f"and hour {bh} and {ah}"
+            )
 
             request = self.basic_request.format(
-                before_time=bt.timestamp(), after_time=at.timestamp(),
-                before_hour=bh.timestamp(), after_hour=ah.timestamp(),
-                other_columns=other_columns, other_where=other_where)
+                columns=columns,
+                before_time=bt.timestamp(),
+                after_time=at.timestamp(),
+                before_hour=bh.timestamp(),
+                after_hour=ah.timestamp(),
+                other_tables=other_tables,
+                other_params=other_params,
+            )
 
-            df = self._impala(request)
+            df = self._impala(request, cached)
 
             if df is None:
                 continue
 
-            df = df.drop(['lastcontact', ], axis=1)
+            df = df.drop(["lastcontact"], axis=1)
 
-            # TODO option for callsign/icao24
-            # TODO remove serials as well (option)
-            df = df.drop_duplicates(('callsign', 'time'))
             if df.lat.dtype == object:
-                df = df[df.lat != 'lat']  # header is regularly repeated
-
+                df = df[df.lat != "lat"]  # header is regularly repeated
             # restore all types
-            for column_name in ['lat', 'lon', 'velocity', 'heading',
-                                'geoaltitude', 'baroaltitude', 'vertrate',
-                                'time', 'lastposupdate']:
+            for column_name in [
+                "lat",
+                "lon",
+                "velocity",
+                "heading",
+                "vertrate",
+                "baroaltitude",
+                "geoaltitude",
+                "lastposupdate",
+                # "lastcontact",
+            ]:
                 df[column_name] = df[column_name].astype(float)
 
+            for column_name in ["time", "hour"]:
+                df[column_name] = df[column_name].astype(int)
+
             df.icao24 = df.icao24.apply(
-                lambda x: "{:0>6}".format(hex(int(str(x), 16))[2:]))
+                lambda x: "{:0>6}".format(hex(int(str(x), 16))[2:])
+            )
 
             if df.onground.dtype != bool:
-                df.onground = (df.onground == 'true')
-                df.alert = (df.alert == 'true')
-                df.spi = (df.spi == 'true')
+                df.onground = df.onground == "true"
+                df.alert = df.alert == "true"
+                df.spi = df.spi == "true"
 
             # better (to me) formalism about columns
-            df = df.rename(columns={'lat': 'latitude',
-                                    'lon': 'longitude',
-                                    'heading': 'track',
-                                    'velocity': 'ground_speed',
-                                    'vertrate': 'vertical_rate',
-                                    'baroaltitude': 'baro_altitude',
-                                    'geoaltitude': 'altitude',
-                                    'time': 'timestamp',
-                                    'lastposupdate': 'last_position'})
+            df = df.rename(
+                columns={
+                    "lat": "latitude",
+                    "lon": "longitude",
+                    "heading": "track",
+                    "velocity": "ground_speed",
+                    "vertrate": "vertical_rate",
+                    "baroaltitude": "baro_altitude",
+                    "geoaltitude": "altitude",
+                    "time": "timestamp",
+                    "lastposupdate": "last_position",
+                }
+            )
 
             df = self._format_dataframe(df)
             cumul.append(df)
@@ -239,9 +304,14 @@ class OpenSky(object):
         return Traffic(pd.concat(cumul))
 
     def sensors(
-            self, before: timelike, after: timelike,
-            bounds: Tuple[float, float, float, float]
+        self,
+        before: timelike,
+        after: timelike,
+        bounds: Tuple[float, float, float, float],
     ) -> Optional[pd.DataFrame]:
+
+        before = to_datetime(before)
+        after = to_datetime(after)
 
         before_hour = round_time(before, "before")
         after_hour = round_time(after, "after")
@@ -253,53 +323,94 @@ class OpenSky(object):
         except AttributeError:
             west, south, east, north = bounds
 
-        other_where = "and lon>={} and lon<={} ".format(west, east)
-        other_where += "and lat>={} and lat<={} ".format(south, north)
+        other_params = "and lon>={} and lon<={} ".format(west, east)
+        other_params += "and lat>={} and lat<={} ".format(south, north)
 
-        query = self.sensor_request.format(before_hour=before_hour,
-                                           after_hour=after_hour,
-                                           other_where=other_where)
+        query = self.basic_request.format(
+            columns="icao24, callsign, s.ITEM as serial, count(*) as count",
+            other_tables=", state_vectors_data4.serials s",
+            before_time=before.timestamp(),
+            after_time=after.timestamp(),
+            before_hour=before_hour.timestamp(),
+            after_hour=after_hour.timestamp(),
+            other_params=other_params + "group by icao24, callsign, s.ITEM",
+        )
 
         logging.info(f"Sending request: {query}")
         df = self._impala(query)
         if df is None:
             return None
 
-        df = df[df['count'] != 'count']
-        df['count'] = df['count'].astype(int)
+        df = df[df["count"] != "count"]
+        df["count"] = df["count"].astype(int)
 
         return df
 
     def online_aircraft(self, own=False) -> pd.DataFrame:
-        what = "own" if (own and self.username != ""
-                         and self.password != "") else "all"
-        c = requests.get(f"https://opensky-network.org/api/states/{what}",
-                         auth=self.auth)
+        what = (
+            "own"
+            if (own and self.username != "" and self.password != "")
+            else "all"
+        )
+        c = requests.get(
+            f"https://opensky-network.org/api/states/{what}", auth=self.auth
+        )
         if c.status_code != 200:
             raise ValueError(c.content.decode())
-        r = pd.DataFrame.from_records(c.json()['states'], columns=self.columns)
-        r = r.drop(['origin_country', 'spi', 'sensors'], axis=1)
+        r = pd.DataFrame.from_records(
+            c.json()["states"], columns=self._json_columns
+        )
+        r = r.drop(["origin_country", "spi", "sensors"], axis=1)
         r = r.dropna()
         return self._format_dataframe(r, nautical_units=True)
 
-    def at_airport(self, before: timelike, after: timelike,
-                   airport: Airport) -> Optional[pd.DataFrame]:
+    def at_airport(
+        self,
+        before: timelike,
+        after: timelike,
+        airport: Union[Airport, str],
+        count: bool = False,
+    ) -> Optional[pd.DataFrame]:
 
         before = to_datetime(before)
         after = to_datetime(after)
 
-        before_hour = round_time(before, how='before')
-        after_hour = round_time(after, how='after')
+        before_hour = round_time(before, how="before")
+        after_hour = round_time(after, how="after")
 
-        request = self.airport_request.format(
+        if isinstance(airport, str):
+            from traffic.data import airports
+
+            airport = cast(Airport, airports[airport])
+
+        other_params = (
+            "and lat<={airport_latmax} and lat>={airport_latmin} "
+            "and lon<={airport_lonmax} and lon>={airport_lonmin} "
+            "and baroaltitude<=1000 "
+            "group by icao24, callsign"
+        ).format(
+            airport_latmax=airport.lat + 0.1,
+            airport_latmin=airport.lat - 0.1,
+            airport_lonmax=airport.lon + 0.1,
+            airport_lonmin=airport.lon - 0.1,
+        )
+
+        columns = "icao24, callsign"
+        other_tables = ""
+        if count is True:
+            columns = "count(*) as count, s.ITEM as serial, " + columns
+            other_tables += ", state_vectors_data4.serials s"
+            other_params += ", s.ITEM"
+
+        request = self.basic_request.format(
+            columns=columns,
             before_time=before.timestamp(),
             after_time=after.timestamp(),
             before_hour=before_hour.timestamp(),
             after_hour=after_hour.timestamp(),
-            airport_latmax=airport.lat + 0.1,
-            airport_latmin=airport.lat - 0.1,
-            airport_lonmax=airport.lon + 0.1,
-            airport_lonmin=airport.lon - 0.1,)
+            other_tables=other_tables,
+            other_params=other_params,
+        )
 
         df = self._impala(request)
 
