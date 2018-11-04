e5c2e6cf0413f1f4b260df9377dfbaa00f95f2fb
Xavier Olive <git@xoolive.org>
2018-11-04 15:47:08 +0100

    basic architecture for a better CLI ref #25

---
+++ traffic/console/__init__.py
@@ -0,0 +1,37 @@
+import argparse
+import logging
+import sys
+
+from . import data, decode, gui, makeapp
+from .. import edit_config
+
+cmd = {
+    'data': data.main,
+    "decode": decode.main,
+    "config": edit_config,
+    "gui": gui.main,
+    "makeapp": makeapp.main,
+}
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="traffic command-line interface",
+        epilog="For specific help about each command, type traffic command -h"
+    )
+
+    parser.add_argument("command", help=f"choose among: {', '.join(cmd.keys())}")
+    parser.add_argument(
+        "args", nargs=argparse.REMAINDER,
+        help="all arguments to dispatch to command"
+    )
+
+    args = parser.parse_args()
+    fun = cmd.get(args.command, None)
+
+    if fun is None:
+        return parser.print_help()
+
+    return fun(args.args)
---
+++ traffic/console/data.py
@@ -0,0 +1,62 @@
+
+import argparse
+
+def get_aircraft(*args):
+    from ..data import aircraft as data_aircraft
+
+    if args[0] == "get":
+        print(data_aircraft[args[1]])
+    elif args[0] == "operator":
+        print(data_aircraft.operator(" ".join(args[1:])))
+    elif args[0] == "stats":
+        print(data_aircraft.stats(" ".join(args[1:])))
+    else:
+        raise RuntimeError("Usage: traffic aircraft [get|operator|stats]")
+
+
+
+def main(args):
+
+    from ..data import airports, aircraft, navaids
+
+    parser = argparse.ArgumentParser(
+        prog="traffic data",
+        description="Explore basic navigational data embedded with traffic"
+    )
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--aircraft", "-a", dest='aircraft', action='store_true',
+            help="followed by registration or transponder identification")
+    group.add_argument("--airport", "-p", dest='airport', action='store_true',
+            help="followed by IATA, ICAO codes, country or city names")
+    group.add_argument("--navaid", "-n", dest='navaid', action='store_true',
+            help="followed by navigational beacon name"
+            )
+    group.add_argument("--operator", "-o", dest='operator', action='store_true',
+            help="followed by the name of the operator (list all aircraft)")
+    group.add_argument("--stats", "-s", dest='stats', action='store_true',
+            help="followed by the name of the operator (stats of all aircraft)")
+
+    parser.add_argument("args", nargs=argparse.REMAINDER)
+
+    args = parser.parse_args(args)
+
+    if args.aircraft:
+        for arg in args.args:
+            print(aircraft[arg])
+
+    if args.operator:
+        print(aircraft.operator(" ".join(args.args)))
+
+    if args.stats:
+        print(aircraft.stats(" ".join(args.args)))
+
+    if args.airport:
+        for arg in args.args:
+            for airport in airports.search(arg):
+                print(airport)
+
+    if args.navaid:
+        for arg in args.args:
+            for navaid in navaids.search(arg):
+                print(navaid)
