a175680f2da5ff04c4b629117c8e8bf7faeb8dd3
Xavier Olive <git@xoolive.org>
2018-11-18 23:38:56 +0100

    javascript import order

--- docs/conf.py
+++ docs/conf.py
@@ -158,3 +158,8 @@ texinfo_documents = [
 
 
 # -- Extension configuration -------------------------------------------------
+
+def setup(app):
+    app.add_javascript("https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.4/require.min.js")
+    app.add_javascript("https://unpkg.com/@jupyter-widgets/html-manager@*/dist/embed-amd.js")
+
