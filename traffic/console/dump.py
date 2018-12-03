b2c2c9a8543217873e2de6493ea215414896f04f
Xavier Olive <git@xoolive.org>
2018-12-03 20:53:58 +0100

    new CLI tool

---
+++ traffic/console/dump.py
@@ -0,0 +1,72 @@
+import argparse
+import logging
+import os
+import signal
+import sys
+from pathlib import Path
+
+
+def main(args):
+
+    parser = argparse.ArgumentParser(
+        prog="traffic dump",
+        description="Get data from a TCP port, timestamp and dump it to a file",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        dest="output",
+        type=Path,
+        default="output.pkl",
+        help="destination file for pickled traffic; (default: output.pkl)",
+    )
+    parser.add_argument(
+        "-v",
+        dest="verbose",
+        action="count",
+        default=0,
+        help="display logging messages",
+    )
+    parser.add_argument(
+        "reference",
+        help="handle in configuration file or IATA/ICAO code for dump1090",
+    )
+
+    args = parser.parse_args(args)
+
+    logger = logging.getLogger()
+    if args.verbose == 1:
+        logger.setLevel(logging.INFO)
+    elif args.verbose >= 2:
+        logger.setLevel(logging.DEBUG)
+
+    from traffic import config
+    from traffic.data import ModeS_Decoder
+
+    try:
+        address = config.get("decoders", args.reference)
+        host_port, reference = address.split("/")
+        host, port = host_port.split(":")
+        decoder = ModeS_Decoder.from_address(
+            host, int(port), reference,
+            args.output.with_suffix(".csv").as_posix()
+        )
+    except Exception:
+        logging.info("fallback to dump1090")
+        decoder = ModeS_Decoder.from_dump1090(
+            args.reference, args.output.with_suffix(".csv").as_posix()
+        )
+
+    def signal_handler(sig, frame):
+        logging.info("Interruption signal caught")
+        t = decoder.traffic
+        if t is not None:
+            t.to_pickle(os.path.expanduser(args.output.with_suffix(".pkl")))
+            logging.info(f"Traffic saved to {args.output}")
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+    logging.info("Press Ctrl+C to quit")
+
+    while True:
+        continue
