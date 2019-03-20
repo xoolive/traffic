b179b7af4c084372e11dc8003ad5e9d1d29339b5
Benoit Viry <benoit.viry@onera.fr>
2019-03-20 14:33:51 +0100

    (value, unit) -> rule 2nd try

--- traffic/core/traffic.py
+++ traffic/core/traffic.py
@@ -381,7 +381,7 @@ class Traffic(GeographyMixin):
         with ProcessPoolExecutor(max_workers=max_workers) as executor:
             cumul = []
             tasks = {
-                executor.submit(flight.resample, value, unit): flight
+                executor.submit(flight.resample, rule): flight
                 for flight in self
             }
             for future in tqdm(as_completed(tasks), total=len(tasks)):
