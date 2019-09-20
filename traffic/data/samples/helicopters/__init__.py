33d9101143627245ea07969f95a8013f3881fe38
Xavier Olive <git@xoolive.org>
2019-09-20 23:35:54 +0200

    fix for types

--- traffic/data/samples/helicopters/__init__.py
+++ traffic/data/samples/helicopters/__init__.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from .. import assign_id, get_flight
+from .. import get_flight
 
 _current_dir = Path(__file__).parent
 __all__ = list(f.stem[:-5] for f in _current_dir.glob("*.json.gz"))
@@ -9,8 +9,7 @@ __all__ = list(f.stem[:-5] for f in _current_dir.glob("*.json.gz"))
 def __getattr__(name: str):
     if name == "traffic":
         return sum(
-            get_flight(name, _current_dir).pipe(lambda x: assign_id(x, name))
-            for name in __all__
+            get_flight(name, _current_dir).assign_id(name) for name in __all__
         )
 
     if name not in __all__:
