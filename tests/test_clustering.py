768c3039cc545f27fb84ccb8ed6e0643bdde5dea
Xavier Olive <git@xoolive.org>
2019-09-21 22:34:42 +0200

    add test for clustering

---
+++ tests/test_clustering.py
@@ -0,0 +1,42 @@
+from cartotools.crs import CH1903p
+
+from traffic.core import Traffic
+from traffic.data.samples import collections, get_sample
+
+
+class StupidClustering:
+    """Special Clustering just for a test without sklearn
+    Dumb clustering assigning first flight to cluster 0,
+    second to 1, etc.
+    """
+
+    def fit(self, X):
+        self.labels_ = [i % 2 for i, _ in enumerate(X.T)]
+
+    def predict(self, X):
+        pass
+
+
+def test_clustering() -> None:
+    switzerland: Traffic = get_sample(collections, "switzerland")
+
+    smaller = (
+        switzerland.between("2018-08-01 12:00", "2018-08-01 14:00")
+        .assign_id()
+        .eval(max_workers=4)
+    )
+
+    t_clustering = smaller.clustering(
+        nb_samples=15,
+        projection=CH1903p(),
+        features=["x", "y"],
+        clustering=StupidClustering(),
+    ).fit_predict()
+
+    v1, v2 = (
+        t_clustering.groupby(["cluster"])
+        .agg({"flight_id": "nunique"})
+        .flight_id
+    )
+
+    assert abs(v1 - v2) <= 1
