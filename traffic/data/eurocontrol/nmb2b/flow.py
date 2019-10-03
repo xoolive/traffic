f87423a399edaf510a1ae19d27b6a1e4ff7776d5
lbasora <luis.basora@onera.fr>
2019-10-03 16:43:01 +0200

    flow regulations improvements

--- traffic/data/eurocontrol/nmb2b/flow.py
+++ traffic/data/eurocontrol/nmb2b/flow.py
@@ -11,15 +11,17 @@ from ....core.time import timelike, to_datetime
 from .reply import B2BReply
 from .xml import REQUESTS
 
+rename_cols = {"id": "tvId", "regulationState": "state", "subType": "type"}
+
 default_regulation_fields: Set[str] = {
-    # "applicability",
+    "applicability",
     # "autolink",
     # "measureCherryPicked",
     # "initialConstraints",
     # "linkedRegulations",
     "location",
     "protectedLocation",
-    # "reason",
+    "reason",
     # "remark",
     "regulationState",
     # "supplementaryConstraints",
@@ -40,6 +42,92 @@ default_regulation_fields: Set[str] = {
 
 # https://github.com/python/mypy/issues/2511
 RegulationListTypeVar = TypeVar("RegulationListTypeVar", bound="RegulationList")
+
+
+class RegulationInfo(B2BReply):
+    @property
+    def regulation_id(self) -> str:
+        assert self.reply is not None
+        elt = self.reply.find("regulationId")
+        assert elt is not None
+        assert elt.text is not None
+        return elt.text
+
+    @property
+    def state(self) -> str:
+        assert self.reply is not None
+        elt = self.reply.find("regulationState")
+        assert elt is not None
+        assert elt.text is not None
+        return elt.text
+
+    @property
+    def type(self) -> str:
+        assert self.reply is not None
+        elt = self.reply.find("subType")
+        assert elt is not None
+        assert elt.text is not None
+        return elt.text
+
+    @property
+    def start(self):
+        assert self.reply is not None
+        elt = self.reply.find("applicability/wef")
+        assert elt is not None
+        assert elt.text is not None
+        return pd.Timestamp(elt.text, tz="UTC")
+
+    @property
+    def stop(self):
+        assert self.reply is not None
+        elt = self.reply.find("applicability/unt")
+        assert elt is not None
+        assert elt.text is not None
+        return pd.Timestamp(elt.text, tz="UTC")
+
+    @property
+    def tvId(self) -> str:
+        assert self.reply is not None
+        elt = self.reply.find("location/id")
+        assert elt is not None
+        assert elt.text is not None
+        return elt.text
+
+    @property
+    def location(self):
+        assert self.reply is not None
+        elt = self.reply.find(
+            "location/referenceLocation-ReferenceLocationAirspace/id"
+        )
+        if elt is not None:
+            return elt.text
+        elt = self.reply.find(
+            "location/referenceLocation-ReferenceLocationAerodrome/id"
+        )
+        if elt is not None:
+            return elt.text
+        return None
+
+    @property
+    def fl_min(self) -> int:
+        assert self.reply is not None
+        elt = self.reply.find("location/flightLevels/min/level")
+        return int(elt.text) if elt is not None else 0
+
+    @property
+    def fl_max(self) -> int:
+        assert self.reply is not None
+        elt = self.reply.find("location/flightLevels/max/level")
+        return int(elt.text) if elt is not None else 999
+
+    def __getattr__(self, name) -> str:
+        cls = type(self)
+        assert self.reply is not None
+        elt = self.reply.find(name)
+        if elt is not None:
+            return elt.text  # type: ignore
+        msg = "{.__name__!r} object has no attribute {!r}"
+        raise AttributeError(msg.format(cls, name))
 
 
 class RegulationList(DataFrameMixin, B2BReply):
@@ -63,15 +151,107 @@ class RegulationList(DataFrameMixin, B2BReply):
         instance.build_df()
         return instance
 
+    def __getitem__(self, item) -> Optional[RegulationInfo]:
+        assert self.reply is not None
+        for elt in self.reply.findall("data/regulations/item"):
+            key = elt.find("regulationId")
+            assert key is not None
+            if key.text == item:
+                return RegulationInfo.fromET(elt)
+
+        return None
+
+    def _ipython_key_completions_(self) -> Set[str]:
+        return set(self.data.RegulationId.unique())
+
     def build_df(self) -> None:
         assert self.reply is not None
 
         self.data = pd.DataFrame.from_records(
             [
-                {**{p.tag: p.text for p in elt if p.text is not None}}
+                {
+                    **{p.tag: p.text for p in elt if p.text is not None},
+                    **{  # type: ignore
+                        p.tag: p.text
+                        for p in elt.find("location")  # type: ignore
+                        if p.text is not None
+                    },
+                    **{  # type: ignore
+                        "start": elt.find(  # type: ignore
+                            "applicability/wef"
+                        ).text  # type: ignore
+                        if elt.find("applicability") is not None
+                        else None,
+                        "stop": elt.find(  # type: ignore
+                            "applicability/unt"
+                        ).text  # type: ignore
+                        if elt.find("applicability") is not None
+                        else None,
+                    },
+                    **{  # type: ignore
+                        "airspace": elt.find(  # type: ignore
+                            "location/referenceLocation-ReferenceLocationAirspace/id"
+                        ).text  # type: ignore
+                        if elt.find(
+                            "location/referenceLocation-ReferenceLocationAirspace"
+                        )
+                        is not None
+                        else None,
+                        "aerodrome": elt.find(  # type: ignore
+                            "location/referenceLocation-ReferenceLocationAerodrome/id"
+                        ).text  # type: ignore
+                        if elt.find(
+                            "location/referenceLocation-ReferenceLocationAerodrome"
+                        )
+                        is not None
+                        else None,
+                    },
+                    **{  # type: ignore
+                        "fl_min": elt.find(  # type: ignore
+                            "location/flightLevels/min/level"
+                        ).text  # type: ignore
+                        if elt.find("location/flightLevels/min/level")
+                        is not None
+                        else 0,
+                        "fl_max": elt.find(  # type: ignore
+                            "location/flightLevels/max/level"
+                        ).text  # type: ignore
+                        if elt.find("location/flightLevels/max/level")
+                        is not None
+                        else 999,
+                    },
+                }
                 for elt in self.reply.findall("data/regulations/item")
             ]
         )
+
+        self.data = self.data.rename(columns=rename_cols)
+        self.data = self.data[
+            [
+                "regulationId",
+                "state",
+                "type",
+                "reason",
+                "start",
+                "stop",
+                "tvId",
+                "airspace",
+                "aerodrome",
+                "fl_min",
+                "fl_max",
+                "description",
+            ]
+        ]
+
+        for feat in ["start", "stop"]:
+            if feat in self.data.columns:
+                self.data = self.data.assign(
+                    **{
+                        feat: self.data[feat].apply(
+                            lambda x: pd.Timestamp(x, tz="utc")
+                        )
+                    }
+                )
 
 
 class Measures:
@@ -94,7 +274,7 @@ class Measures:
             start=start,
             stop=stop,
             requestedRegulationFields=(
-                "<requestedRegulationFields>\n"
+                "<requestedRegulationFields>"
                 + "\n".join(
                     f"<item>{field}</item>"
                     for field in default_regulation_fields.union(fields)
@@ -102,11 +282,12 @@ class Measures:
                 + "</requestedRegulationFields>"
             ),
             tvs=(
-                "<tvs>\n"
+                "<tvs>"
                 + "\n".join(f"<item>{tv}</item>" for tv in tvs)
                 + "</tvs>"
-            ),
+            )
+            if tvs is not None
+            else "",
         )
-        print(data)
         rep = self.post(data)  # type: ignore
         return RegulationList.fromB2BReply(rep)
