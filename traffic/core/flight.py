270fd7450380e3aeedd20dd0994fdb33fde90a43
Xavier Olive <git@xoolive.org>
2020-01-28 09:34:39 +0100

    fix F811

--- traffic/core/flight.py
+++ traffic/core/flight.py
@@ -760,8 +760,8 @@ class Flight(HBoxMixin, GeographyMixin, ShapelyMixin):
     def split(self, value: int, unit: str) -> Iterator["Flight"]:
         ...
 
-    @overload
-    def split(  # noqa: F811
+    @overload  # noqa: F811
+    def split(
         self, value: str, unit: None = None
     ) -> Iterator["Flight"]:
         ...
@@ -1294,8 +1294,8 @@ class Flight(HBoxMixin, GeographyMixin, ShapelyMixin):
     ) -> "Flight":
         ...
 
-    @overload
-    def distance(  # noqa: F811
+    @overload  # noqa: F811
+    def distance(
         self, other: "Flight", column_name: str = "distance"
     ) -> Optional[pd.DataFrame]:
         ...
