6c44a0a2670803adbd0e94f7b2cca75f62545956
Xavier Olive <git@xoolive.org>
2021-08-30 15:22:56 +0200

    move credentials to conftest.py

--- tests/test_impala.py
+++ tests/test_impala.py
@@ -1,7 +1,4 @@
-import os
-import shutil
 from datetime import timedelta
-from pathlib import Path
 from typing import Optional, cast
 
 import pytest
@@ -9,19 +6,6 @@ import pytest
 from traffic.core import Traffic
 from traffic.data import opensky
 from traffic.data.samples import belevingsvlucht, lfbo_tma
-
-# Some of the tests rely on data which is no longer available on Opensky Impala
-# shell at the time
-cache_dir = Path(__file__).parent.parent / "data" / "opensky_cache"
-for p in cache_dir.glob("*"):
-    new_path = opensky.cache_dir / p.name
-    shutil.copy(str(p), str(new_path))  # str necessary for Python <= 3.7
-
-# When new tests are implemented, we should be able to run them on CI
-username = os.environ.get("OPENSKY_USERNAME", None)
-password = os.environ.get("OPENSKY_PASSWORD", None)
-if username is not None and password is not None:
-    opensky.auth = (username, password)
 
 
 def test_flightlist():
