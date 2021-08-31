d1eda44f6496ab31344421c81f122171fe65ed55
Enrico Spinielli <enrico.spinielli@gmail.com>
2021-08-31 16:55:43 +0200

    improved asserts for 'complex queries' tests

--- tests/test_impala.py
+++ tests/test_impala.py
@@ -69,32 +69,40 @@ def test_complex_queries():
             stop="2021-08-24 01:00",
             airport="ESSA",
             arrival_airport="EGLL",
-            limit=3,
         )
-
+    # test that `limit` generate correct query
     t2 = opensky.history(
         start="2021-08-24 00:00",
         stop="2021-08-24 01:00",
         airport="ESSA",
         limit=3,
     )
-    assert t2 is not None
+    assert len(t2.data) == 3
+
+    t2_1 = opensky.history(
+        start="2021-08-24 09:00",
+        stop= "2021-08-24 09:10",
+        airport="ESSA",
+    )
+    assert len(t2_1) == 23
 
     t3 = opensky.history(
-        start="2021-08-24 00:00",
-        stop="2021-08-24 01:00",
+        start="2021-08-24 09:00",
+        stop= "2021-08-24 09:10",
         arrival_airport="ESSA",
-        limit=3,
     )
-    assert t3 is not None
+    assert len(t3) == 13
 
     t4 = opensky.history(
-        start="2021-08-24 00:00",
+        start="2021-08-24 11:32",
+        stop= "2021-08-24 11:42",
         departure_airport="ESSA",
         arrival_airport="EGLL",
-        limit=3,
     )
-    assert t4 is not None
+    assert len(t4) == 1
+    flight = t4["BAW777C"]
+    assert flight is not None
+    assert flight.icao24 == "400936"    
 
     with pytest.raises(RuntimeError, match=error_msg):
         _ = opensky.history(
@@ -107,78 +115,92 @@ def test_complex_queries():
 
     t6 = opensky.history(
         start="2021-08-24 00:00",
-        stop="2021-08-24 01:00",
+        stop= "2021-08-24 00:10",
         arrival_airport="ESSA",
         serials=-1408232560,
-        limit=3,
     )
-    assert t6 is not None
+    assert len(t6) == 1
+    flight = t6[0]
+    assert flight is not None
+    assert flight.callsign == "SAS6906"    
+    assert flight.icao24 == "4ca863"    
 
     t7 = opensky.history(
         start="2021-08-24 00:00",
-        stop="2021-08-24 01:00",
+        stop= "2021-08-24 00:10",
         serials=(-1408232560, -1408232534),
-        limit=3,
     )
-    assert t7 is not None
+    assert len(t7) == 12
 
     t8 = opensky.history(
-        start="2021-08-24 00:00",
-        stop="2021-08-24 01:00",
+        start="2021-08-24 09:00",
+        stop= "2021-08-24 09:10",
         departure_airport="ESSA",
         serials=(-1408232560, -1408232534),
-        limit=3,
     )
-    assert t8 is not None
+    assert len(t8) == 1
+    flight = t8[0]
+    assert flight is not None
+    assert flight.callsign == "LOT454"    
+    assert flight.icao24 == "489789"    
 
     t9 = opensky.history(
-            start="2021-08-24 00:00",
-            stop="2021-08-24 01:00",
-            bounds=[17.8936, 59.6118, 17.9894, 59.6716],
-            serials=(-1408232560, -1408232534),
-            limit=3,
-        )
-    assert t9 is not None
+        start="2021-08-24 09:00",
+        stop= "2021-08-24 09:10",
+        bounds=[17.8936, 59.6118, 17.9894, 59.6716],
+        serials=(-1408232560, -1408232534),
+    )
+    assert len(t9) == 9
+    flight = t9["SAS1136"]
+    assert flight is not None
+    assert flight.icao24 == "51110b"    
 
     tA = opensky.history(
-            start="2021-08-24 00:00",
-            stop="2021-08-24 01:00",
-            departure_airport="ESSA",
-            bounds=[17.8936, 59.6118, 17.9894, 59.6716],
-            serials=(-1408232560, -1408232534),
-            limit=3,
-        )
-    assert tA is not None
+        start="2021-08-24 09:30",
+        stop= "2021-08-24 09:40",
+        departure_airport="ESSA",
+        bounds=[17.8936, 59.6118, 17.9894, 59.6716],
+        serials=(-1408232560, -1408232534),
+    )
+    assert len(tA) == 1
+    flight = tA[0]
+    assert flight is not None
+    assert flight.callsign == "THY5HT"    
+    assert flight.icao24 == "4bb1c5"    
 
     tB = opensky.history(
-            start="2021-08-24 00:00",
-            stop="2021-08-24 01:00",
-            departure_airport="ESSA",
-            count=True,
-            bounds=[17.8936, 59.6118, 17.9894, 59.6716],
-            serials=(-1408232560, -1408232534),
-            limit=3,
-        )
-    assert tB is not None
+        start="2021-08-24 09:45",
+        stop= "2021-08-24 09:50",
+        departure_airport="ESSA",
+        count=True,
+        bounds=[17.8936, 59.6118, 17.9894, 59.6716],
+        serials=(-1408232560, -1408232534),
+    )
+    assert len(tB) == 1
+    flight = tB[0]
+    assert flight is not None
+    assert flight.callsign == "SAS69E"    
+    assert flight.icao24 == "4ac9e5"    
 
     tC = opensky.history(
-            start="2021-08-24 00:00",
-            stop="2021-08-24 01:00",
-            departure_airport="ESSA",
-            count=True,
-            bounds=[17.8936, 59.6118, 17.9894, 59.6716],
-            limit=3,
-        )
-    assert tC is not None
+        start="2021-08-24 09:45",
+        stop= "2021-08-24 09:50",
+        departure_airport="ESSA",
+        count=True,
+        bounds=[17.8936, 59.6118, 17.9894, 59.6716],
+    )
+    assert len(tC) == 1
+    flight = tC[0]
+    assert flight is not None
+    assert flight.callsign == "SAS69E"    
+    assert flight.icao24 == "4ac9e5"    
 
     tD = opensky.history(
-            start="2021-08-24 00:00",
-            stop="2021-08-24 01:00",
-            count=True,
-            bounds=[17.8936, 59.6118, 17.9894, 59.6716],
-            limit=3,
-        )
-    assert tD is not None
+        start="2021-08-24 09:45",
+        stop= "2021-08-24 09:50",
+        bounds=[17.8936, 59.6118, 17.9894, 59.6716],
+    )
+    assert len(tD) == 9
 
 def test_rawdata():
 
