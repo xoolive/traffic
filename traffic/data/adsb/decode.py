0fd428692cc705dcec1ddab9368467d75f1c9663
Xavier Olive <git@xoolive.org>
2021-11-25 14:52:09 +0100

    decode raw files according to different templates

--- traffic/data/adsb/decode.py
+++ traffic/data/adsb/decode.py
@@ -858,11 +858,50 @@ class AircraftDict(Dict[str, Aircraft]):
             ac.lon0 = lon0
 
 
+class DumpFormat:
+    def __init__(self, template: str, sep: str = ",") -> None:
+
+        self.template = template
+        self.sep = sep
+        self.cols = list(x.strip() for x in template.split(sep))
+        time_gen = (i for i, elt in enumerate(self.cols) if elt == "time")
+        self.time_index = next(time_gen, None)
+        if self.time_index is None:
+            msg = "Format invalid: must contain 'time'"
+            raise ValueError(msg)
+
+        long_gen = (i for i, elt in enumerate(self.cols) if elt == "longmsg")
+        self.msg_index = next(long_gen, None)
+        self.splitmsg = slice(18, None)
+
+        if self.msg_index is not None:
+            return
+
+        short_gen = (i for i, elt in enumerate(self.cols) if elt == "shortmsg")
+        self.msg_index = next(short_gen, None)
+        if self.msg_index is None:
+            msg = "Format invalid: must contain either 'longmsg' or 'shortmsg'"
+            raise ValueError(msg)
+
+        self.splitmsg = slice(None)
+
+    def get_timestamp(self, line: str) -> datetime:
+        elts = line.split(self.sep)
+        return datetime.fromtimestamp(
+            float(elts[self.time_index]), timezone.utc  # type: ignore
+        )
+
+    def get_msg(self, line: str) -> str:
+        elts = line.split(self.sep)
+        return elts[self.msg_index][self.splitmsg]  # type: ignore
+
+
 class Decoder:
     thread: Optional[StoppableThread]
 
     def __init__(
-        self, reference: Union[None, str, Airport, Tuple[float, float]] = None
+        self,
+        reference: Union[None, str, Airport, Tuple[float, float]] = None,
     ) -> None:
         if isinstance(reference, str):
             from ...data import airports
@@ -889,10 +928,14 @@ class Decoder:
         filename: Union[str, Path],
         reference: Union[str, Airport, Tuple[float, float]],
         uncertainty: bool = False,
+        template: str = "time, longmsg",
+        sep: str = ",",
     ) -> "Decoder":
 
         if isinstance(filename, str):
             filename = Path(filename)
+
+        dumpformat = DumpFormat(template, sep)
 
         with filename.open("r") as fh:
             all_lines = fh.readlines()
@@ -900,10 +943,8 @@ class Decoder:
             decoder.process_msgs(
                 list(
                     (
-                        datetime.fromtimestamp(
-                            float(line.strip().split(",")[0]), timezone.utc
-                        ),
-                        line.strip().split(",")[1][18:],
+                        dumpformat.get_timestamp(line),
+                        dumpformat.get_msg(line),
                     )
                     for line in all_lines
                 ),
