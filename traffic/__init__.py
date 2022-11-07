be2f7476a3000782d19d9bb95dbbe5d9ad7c4b45
Xavier Olive <git@xoolive.org>
2022-11-07 12:12:16 +0100

    fix documentation

--- traffic/__init__.py
+++ traffic/__init__.py
@@ -9,6 +9,8 @@ from appdirs import user_cache_dir, user_config_dir
 
 import pandas as pd
 
+from . import drawing  # noqa: F401
+
 __version__ = version("traffic")
 __all__ = ["config_dir", "config_file", "cache_dir"]
 
