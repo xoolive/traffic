7928cb8f8ddff254ff58047ec2d624e82bbb40e0
Vibujor <enceladael@gmail.com>
2023-11-17 11:16:15 +0000

    - comments

--- scripts/extract_deviations_2.py
+++ scripts/extract_deviations_2.py
@@ -39,7 +39,6 @@ class Metadata(DataFrameMixin):
         return FlightPlan(df.iloc[0]["route"])
 
 
-# metadata = cycle airac complet
 metadata = pd.read_parquet("A2207_old.parquet")
 
 metadata_simple = Metadata(
@@ -48,20 +47,8 @@ metadata_simple = Metadata(
     .eval("icao24 = icao24.str.lower()")
 )
 
-# SEPARATION
-
-
-# t2 = Traffic.from_file("t2_0722_noonground2.parquet")
 t2 = Traffic.from_file("test_format_data_1.parquet")
-# t2 = t2[:1000]
-# assert t2 is not None
-# t2 = t2.query(f"altitude>{altitude_min}")
-# assert t2 is not None
-# t2 = t2.clip(aixm_airspaces[extent]).filter().resample("1s").eval(max_workers=8)
 assert t2 is not None
-
-# SEPARATION
-print("post chargement t2")
 
 
 def dist_lat_min(f1: Flight, f2: Flight) -> Any:
@@ -78,15 +65,6 @@ def dist_lat_min(f1: Flight, f2: Flight) -> Any:
         return None
 
 
-# SEPARATION
-
-
-# 7 -> with neighbors same for pred and flown
-# 11 -> no resampling
-# list_dicts = []
-# ids_error = []
-# [["AA38888453"]]
-# verification min AA38882731
 probleme = t2["AA39472649"]
 assert probleme is not None
 t2 = t2 - probleme
@@ -96,7 +74,7 @@ nbworkers = 60
 nt2 = len(t2)
 nbsubsets = nt2 // nbworkers
 print("before subsets2")
-# POUR LE BUG
+
 subsetst2 = [
     t2[nbsubsets * i : nbsubsets * (i + 1)]
     if i < nbworkers - 1
@@ -106,7 +84,6 @@ subsetst2 = [
 
 couples_datas = [
     (sub, f"test_extract_deviations/stats_para_{i}.parquet")
-    # (sub, f"test_extract_deviations/stats_para_{i}.parquet")
     for i, sub in enumerate(subsetst2)
 ]
 
@@ -142,7 +119,7 @@ def traitement(info: Tuple[Traffic, str]) -> None:
 
                     flmin = flight_trou.altitude_min - margin_fl
                     flmax = flight_trou.altitude_max + margin_fl
-                    # START CREATING NEIGHBOURS
+
                     stop_voisins = min(
                         flight_trou.start + pd.Timedelta(minutes=forward_time),
                         flight.stop,
@@ -151,12 +128,7 @@ def traitement(info: Tuple[Traffic, str]) -> None:
                         flight_trou.start, stop_voisins
                     )
                     assert flight_interest is not None
-                    # we only want the stable portion of flight_interest :
-                    # flight_interest = flight_interest.query(
-                    #     f"{flmin}<=altitude<={flmax}"
-                    # )
-                    # assert flight_interest is not None
-                    # find potential off-limits portion(s) in terms of altitude
+
                     offlimits = flight_interest.query(
                         f"altitude>{flmax} or altitude<{flmin}"
                     )
@@ -176,10 +148,9 @@ def traitement(info: Tuple[Traffic, str]) -> None:
                         .iterate_lazy()
                         .query(f"{flmin} <= altitude <= {flmax}")
                         .feature_gt("duration", datetime.timedelta(seconds=2))
-                        # .resample("1s")
                         .eval()
                     )
-                    # STOP CREATING NEIGHBOURS
+
                     pred_possible = flight.before(flight_trou.start) is not None
 
                     if voisins is None and not pred_possible:
@@ -201,11 +172,7 @@ def traitement(info: Tuple[Traffic, str]) -> None:
 
                     if pred_possible:
                         # compute prediction
-                        # pred = (
-                        #     flight.before(flight_trou.start)  # type: ignore
-                        #     .forward(minutes=forward_time)
-                        #     .resample("1s")
-                        # )
+
                         pred_fp = predict_fp(
                             flight,
                             metadata_simple[id],
@@ -214,24 +181,6 @@ def traitement(info: Tuple[Traffic, str]) -> None:
                             minutes=forward_time,
                             min_distance=min_distance,
                         )
-
-                        # angle computation (max and 90% quantile)
-                        # angle_flown = flight_trou.data.set_index("timestamp")[
-                        #     "track"
-                        # ]
-                        # angle_pred = pred.data.set_index("timestamp")["track"]
-                        # d = pd.DataFrame((angle_flown - angle_pred) % 360)
-                        # d.loc[d["track"] < -180, "track"] = d["track"] + 360
-                        # d.loc[d["track"] >= 180, "track"] = d["track"] - 360
-                        # temp_dict["max_dev_angle"] = d.track.abs().max()
-                        # temp_dict["q90_dev_angle"] = (
-                        #     d.track.abs().quantile([0.9]).iloc[0]
-                        # )
-
-                        # # total distance (sum of lateral pred-flown)
-                        # temp_dict["deviation_area"] = flight_trou.distance(
-                        #     pred
-                        # ).lateral.sum()
 
                         # zero neighbors
                         temp_dict["nb_voisins"] = 0
@@ -252,32 +201,7 @@ def traitement(info: Tuple[Traffic, str]) -> None:
                             df_dist.lateral == df_dist.lateral.min()
                         ].timestamp.iloc[0]
 
-                        # length of neighbor deviation
-                        # temp_dict["min_f_dev"] = pd.Timedelta(
-                        #     seconds=sum(
-                        #         f.duration.total_seconds()
-                        #         for f in (
-                        #             voisins[idmin_f]
-                        #             - voisins[idmin_f].aligned_on_navpoint(
-                        #                 metadata_simple[idmin_f],
-                        #                 angle_precision,
-                        #                 min_distance=min_distance,
-                        #             )
-                        #         )
-                        #     )
-                        # )
                         if pred_possible:
-                            # we use the same neighbor to compute predicted distance
-                            # df_dist_forward = pred.distance(voisins[idmin_f])
-                            # temp_dict["min_p_id"] = idmin_f
-                            # temp_dict[
-                            #     "min_p_dist"
-                            # ] = df_dist_forward.lateral.min()
-                            # temp_dict["min_p_time"] = df_dist_forward.loc[
-                            #     df_dist_forward.lateral
-                            #     == df_dist_forward.lateral.min()
-                            # ].timestamp.iloc[0]
-
                             df_dist_fp = pred_fp.distance(voisins[idmin_f])
                             temp_dict["min_fp_id"] = idmin_f
                             temp_dict["min_fp_dist"] = df_dist_fp.lateral.min()
