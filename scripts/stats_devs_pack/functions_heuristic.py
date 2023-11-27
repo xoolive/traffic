8413dc3145893e115382486db301ad243c72d0f6
Vibujor <enceladael@gmail.com>
2023-11-27 14:02:39 +0000

    solve errors

--- scripts/stats_devs_pack/functions_heuristic.py
+++ scripts/stats_devs_pack/functions_heuristic.py
@@ -1,38 +1,53 @@
 from datetime import timedelta
+from traffic.core.flight import Position
 from traffic.core.flightplan import _Point
 from traffic.core.geodesy import distance  # distance in meters
-from traffic.core.traffic import Flight
+from traffic.core import Flight
 from traffic.core import FlightPlan
 import pandas as pd
 from traffic.data import aixm_navaids
+from typing import List, Union, Dict, cast
 
 
 def predict_fp(
     flight: Flight,
     fp: FlightPlan,
-    start: str | pd.Timestamp,
-    stop: str | pd.Timestamp,
-    minutes: str = 15,  # REPLACE WITH INT
+    start: Union[str, pd.Timestamp],
+    # stop: str | pd.Timestamp,
+    stop: Union[str, pd.Timestamp],
+    minutes: int = 15,  # REPLACE WITH INT
     angle_precision: int = 2,
     min_distance: int = 150,
-):
-    data_points = {"latitude": [], "longitude": [], "timestamp": []}
+) -> Flight:
+    data_points: Dict[str, List[Union[float, str, pd.Timestamp]]] = {
+        "latitude": [],
+        "longitude": [],
+        "timestamp": [],
+    }
     # print(f"start = {start}\nstop = {stop}\n")
     assert flight is not None
     trou = flight.between(start, stop, strict=False)
     assert trou is not None
-    section = flight.before(trou.start, strict=False).last(minutes=20)
+    section = cast(Flight, flight.before(trou.start, strict=False)).last(
+        minutes=20
+    )
     assert section is not None
     gs = section.groundspeed_mean * 0.514444  # conversion to m/s
 
     data_points["latitude"].append(
-        flight.before(trou.start).at_ratio(1).latitude
+        cast(
+            Position, cast(Flight, flight.before(trou.start)).at_ratio(1)
+        ).latitude
     )
     data_points["longitude"].append(
-        flight.before(trou.start).at_ratio(1).longitude
+        cast(
+            Position, cast(Flight, flight.before(trou.start)).at_ratio(1)
+        ).longitude
     )
     data_points["timestamp"].append(
-        flight.before(trou.start).at_ratio(1).timestamp
+        cast(
+            Position, cast(Flight, flight.before(trou.start)).at_ratio(1)
+        ).timestamp
     )
 
     navaids = fp.all_points
@@ -42,15 +57,19 @@ def predict_fp(
         fp, angle_precision=angle_precision, min_distance=min_distance
     ).final()
 
-    start_nav_name = g.data.navaid.iloc[0]
+    start_nav_name = cast(Flight, g).data.navaid.iloc[0]
     start_nav = next(
         (point for point in navaids if point.name == start_nav_name), None
     )
-    start_index = navaids.index(start_nav)
+    start_index = navaids.index(cast(_Point, start_nav))
     reste_navaids = navaids[start_index:]
     point_depart = _Point(
-        lat=flight.before(trou.start).at_ratio(1).latitude,
-        lon=flight.before(trou.start).at_ratio(1).longitude,
+        lat=cast(
+            Position, cast(Flight, flight.before(trou.start)).at_ratio(1)
+        ).latitude,
+        lon=cast(
+            Position, cast(Flight, flight.before(trou.start)).at_ratio(1)
+        ).longitude,
         name=start_nav_name,
     )
     new_timestamp = trou.start
@@ -81,7 +100,7 @@ def predict_fp(
         **data_points,
         "icao24": flight.icao24,
         "callsign": flight.callsign,
-        "altitude": flight.at(trou.start).altitude,
+        "altitude": cast(Position, flight.at(trou.start)).altitude,
         "flight_id": flight.flight_id,
     }
     return (
