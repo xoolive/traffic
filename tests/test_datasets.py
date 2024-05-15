5ee4edae736d91af409eff3e8235bf05bf6a4e1b
Niclas Wüstenbecker <niclas.wuestenbecker@dlr.de>
2024-05-15 13:56:09 +0200

    add test cases for loading weather and waypoints in SCAT

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,8 +1,20 @@
 from traffic.data.datasets.scat import SCAT
 
+import pandas as pd
+
 
 def test_scat() -> None:
     s = SCAT("scat20161015_20161021.zip", nflights=10)
     assert len(s.traffic) == 10
     assert s.flight_plans.flight_id.nunique() == 10
     assert s.clearances.flight_id.nunique() == 10
+
+
+def test_scat_waypoints() -> None:
+    s = SCAT("scat20161015_20161021.zip", nflights=10, include_waypoints=True)
+    assert isinstance(s.waypoints, pd.DataFrame)
+
+
+def test_scat_weather() -> None:
+    s = SCAT("scat20161015_20161021.zip", nflights=10, include_weather=True)
+    assert isinstance(s.weather, pd.DataFrame)
