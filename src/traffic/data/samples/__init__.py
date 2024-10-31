794c5f3407d2e192d742910f38dc06b5efdbb01b
Xavier Olive <git@xoolive.org>
2024-10-31 16:11:12 +0100

    remove sample_so6

--- src/traffic/data/samples/__init__.py
+++ src/traffic/data/samples/__init__.py
@@ -8,12 +8,10 @@ from typing import Any, Union, cast
 import pandas as pd
 
 from ...core import Airspace, Flight, Traffic
-from ..eurocontrol.ddr.so6 import SO6
 
 _current_dir = Path(__file__).parent
 __all__ = [
     *sorted(f.stem[:-5] for f in _current_dir.glob("**/*.json.gz")),
-    "sample_so6",
     "sample_dump1090",
 ]
 
@@ -68,7 +66,6 @@ lfbo_tma: Airspace
 noisy: Flight
 quickstart: Traffic
 sample_dump1090: Path
-sample_m3: SO6
 switzerland: Traffic
 texas_longhorn: Flight
 zurich_airport: Traffic
@@ -78,8 +75,6 @@ zurich_airport: Traffic
 def __getattr__(name: str) -> Any:
     if name == "sample_dump1090":
         return Path(_current_dir / "dump1090" / "sample_dump1090.bin")
-    if name == "sample_m3":
-        return SO6.from_file(_current_dir / "so6" / "sample_m3.so6.7z")
     if name == "lfbo_tma":
         return Airspace.from_file(_current_dir / "airspaces" / "LFBOTMA.json")
     filelist = list(_current_dir.glob(f"**/{name}.json.gz"))
