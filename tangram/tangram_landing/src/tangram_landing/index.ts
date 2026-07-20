aee9ba062051b6b5619b5c486be2b6e24f66a1fb
Abraham Cheung <58929011+abc8747@users.noreply.github.com>
2026-07-20 20:07:06 +0800

    refactor: use new `SelectionApi`, `TrajectoryApi`, `BusApi` (#531)

--- tangram/tangram_landing/src/tangram_landing/index.ts
+++ tangram/tangram_landing/src/tangram_landing/index.ts
@@ -1,6 +1,13 @@
-import type { TangramApi } from "@open-aviation/tangram-core/api";
+import type { PluginContext } from "@open-aviation/tangram-core/api";
 import AircraftLanding from "./AircraftLanding.vue";
 
-export function install(api: TangramApi) {
-  api.ui.registerWidget("aircraft-align-widget", "MapOverlay", AircraftLanding);
+export function install(ctx: PluginContext) {
+  ctx.api.ui.registerWidget(
+    "aircraft-align-widget",
+    "MapOverlay",
+    AircraftLanding,
+    {
+      pluginId: ctx.id,
+    },
+  );
 }
